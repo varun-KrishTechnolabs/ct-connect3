dc90636a91e3ec02c425debcb90ad1764697c764
varun-KrishTechnolabs <varun.shah@krishtechnolabs.com>
2023-12-08 12:38:24 +0530

    thrid app first commit

--- service/src/routes/service.route.js
+++ service/src/routes/service.route.js
@@ -3,7 +3,7 @@ import { logger } from '../utils/logger.utils.js';
 
 const serviceRouter = Router();
 
-serviceRouter.get('/', async (req, res) => {
+serviceRouter.post('/', async (req, res) => {
   logger.info('Cart update extension executed');
   res.status(200);
   res.send('Hello world from the service of the connector...!!!');
